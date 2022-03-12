98ac37d15a03d9177fc669eede0f31818d0cad9a
Xabier SdC <xabiersdc@gmail.com>
2022-03-12 11:59:13 +0100

    Create function to parse project Makefile

--- src/parse.py
+++ src/parse.py
@@ -1,7 +1,7 @@
-"""Parse python source files."""
+"""Utils to parse project files."""
 
 import ast
-
+import re
 
 def get_variable_from_file(fname: str, varname: str):
     """Parse a python script to get the value of a variable."""
@@ -18,3 +18,42 @@ def get_variable_from_file(fname: str, varname: str):
                     raise ValueError(f"Can't evaluate {varname}.") from None
 
     raise ValueError(f"Variable {varname} not found.")
+
+
+def parse_project_makefile(makefile: str):
+    """Crappy makefile parser.
+    Returns a list of dictionaries, each dictionary contains
+    targets, prerequisites and recipes
+
+    targets : prerequisites
+        recipe
+    """
+    with open(makefile, "r", encoding="utf-8") as f:
+        contents = f.read()
+
+    # join logical lines (backslash escaped lines)
+    contents = re.sub(r"\\\n", "", contents)
+    contents = iter(contents.split("\n"))
+
+    rules = []
+    line = next(contents)
+    try:
+        while True:
+            # match 'targets : prerequisites' line
+            match = re.search(r"^([\w\-./ ]+) ?:(.*)", line)
+            if match:
+                rule = {"targets": match.group(1).split(),
+                        "prerequisites": match.group(2).split()}
+                recipes = []
+                # scan next lines for recipes starting with \t
+                line = next(contents)
+                while line.startswith("\t"):
+                    recipes.append(line[1:])
+                    line = next(contents)
+                rule["recipes"] = recipes
+                rules.append(rule)
+            else:
+                line = next(contents)
+    except StopIteration:
+        pass
+    return rules
