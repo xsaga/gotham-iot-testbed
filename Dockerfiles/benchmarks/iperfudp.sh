c80356f5feba9a8fa2e578401fad68102582ff20
Xabier SdC <xabiersdc@gmail.com>
2022-06-06 19:52:52 +0200

    Update iperfudp.sh

--- Dockerfiles/benchmarks/iperfudp.sh
+++ Dockerfiles/benchmarks/iperfudp.sh
@@ -1,11 +1,11 @@
 # tc qdisc add dev eth0 root netem rate 100mbit
 
-for bw in {0..120000000..10000000}
+for bw in {0..120000000..1000000}
 do
     if [[ $bw -eq 0 ]]; then
         continue
     fi
-    for ps in 64 128 256 512 1024 1280 1448
+    for ps in 512 1024 1280 1448
     do
         echo "BW = $bw  PS = $ps"
         iperf3 -V -c 192.168.0.20 -t 30 -l $ps -b $bw -u > iperfudp_limit100mbps_bw${bw}_ps${ps}.txt
