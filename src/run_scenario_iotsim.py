2368ee08ec1d6eddf904a0633035bdcec0466eae
Xabier SdC <xabiersdc@gmail.com>
2022-09-22 12:51:10 +0200

    Include comments to start packet capturing and stopping the scenario

--- src/run_scenario_iotsim.py
+++ src/run_scenario_iotsim.py
@@ -39,8 +39,25 @@ docker_client.ping()
 # Run #
 #######
 
-start_all_routers(server, project)
+start_all_routers(server, project, sleeptime=30)
 start_all_switches(server, project)
+
+# Start packet capturing in certain links
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-13", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-14", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-15", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-16", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-18", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-19", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-20", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-21", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-23", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-24", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-25", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-26", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-28", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-29", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# start_capture_all_iot_links(server, project, re.compile("openvswitch-30", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
 
 all_iot = get_nodes_id_by_name_regexp(server, project, re.compile("iotsim-.*", re.IGNORECASE))
 
@@ -70,3 +87,45 @@ for n in sorted(iot_rest, key=lambda x: x.name):
 mirai_bot = next(filter(lambda i: i.name == "iotsim-mirai-bot-1", all_iot))
 container_mirai_bot = docker_client.containers.get(get_node_docker_container_id(server, project, mirai_bot.id))
 container_mirai_bot.update(mem_limit="512m", memswap_limit=-1, cpuset_cpus="0", cpu_period=100000, cpu_quota=10000)
+
+
+##############################################################
+# Stop the scenario: stop packet capturing and all the nodes #
+##############################################################
+
+# # stop_capture_all_iot_links(server, project, re.compile("openvswitch.*", re.IGNORECASE), re.compile("iotsim-ip-camera-museum-*", re.IGNORECASE))
+# # stop_capture_all_iot_links(server, project, re.compile("openvswitch.*", re.IGNORECASE), re.compile("iotsim-ip-camera-street-*", re.IGNORECASE))
+# # stop_capture_all_iot_links(server, project, re.compile("openvswitch.*", re.IGNORECASE), re.compile("iotsim-stream-consumer-*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-13", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-14", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-15", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-16", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-18", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-19", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-20", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-21", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-23", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-24", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-25", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-26", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-28", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-29", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+# stop_capture_all_iot_links(server, project, re.compile("openvswitch-30", re.IGNORECASE), re.compile("iotsim.*", re.IGNORECASE))
+
+# for n in sorted(iot_rest, key=lambda x: x.name):
+#     print(f"Stopping {n.name}")
+#     stop_node(server, project, n.id)
+#     time.sleep(0.2)
+
+# for n in iot_servers:
+#     print(f"Stopping {n.name}")
+#     stop_node(server, project, n.id)
+#     time.sleep(0.2)
+
+# for n in general_services:
+#     print(f"Stopping {n.name}")
+#     stop_node(server, project, n.id)
+#     time.sleep(0.2)
+
+# stop_all_switches(server, project)
+# stop_all_routers(server, project)
